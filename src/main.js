2cc21df23ff19c16b8d0a23e94b7f37cc5666881
Josce Jaimes <nanayhachi94@gmail>
2018-09-14 10:51:38 -0500

    refactorizacion version1

--- src/main.js
+++ src/main.js
@@ -1,88 +1,131 @@
-// aqui se va a implementar las funciones
+// // aqui se va a implementar las funciones
 
 const files = require('fs');
-//llamo a la api
+// //llamo a la api
 const path = require('path');
+const fetch = require('node-fetch');
 
-// const validate = (err, url) => {
-//     console.log(url);
-    
-// // if (err){
-// //     console.log('error');
-// // }else(url =  )
 
-// }
+const objectUrl
 
-// const validateUrl = (err,newUrl) => {
-//     console.log(newUrl);
-    
-// }
-// console.log(newUrl);
-
-function validate(newUrl){
-   
-    
-    newUrl.forEach(element =>{
-        console.log(element);
-    //   fetch(element).then(function(response){
-    //       console.log(response.status);
-          
-
-      });
-        
-    
-    
-// fetch(newUrl).then(response => console.log(response.status))
-
+const lineCount = (data, error)=>{ 
+    newArray = [];    
+     let lines = data.split('\n');
+        lines.forEach(element =>{        
+      const urls = element.match(new RegExp(/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g));
+      const url = urls !== null ? newArray.push(urls) :console.log('');
+        });    
+        console.log(newArray);
+        objectUrl(newArray)
+        return newArray
 }
-const lineCount = (urlResult) =>{
-    let newUrl = []; 
-    files.readFile(urlResult,'utf8',(err, data) =>{
-  if (err){
-      console.log(err);
-      return 'error';
-  }else{
-      //saltos de linea \n
-      const lines = data.split('\n');
-      lines.forEach(element => {
-      const expresion = new RegExp(/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g);
-      while(element.match(expresion) !== null){  
-        newUrl.push(element.match(expresion));
-        return newUrl;
-         }              
-        }); 
-    //   console.log(newUrl);
-    //   validateUrl(newUrl);
-    validate(newUrl);
-       return lines + ' '+'lines'     
+
+const readDoc= (pathRout, error) =>{
+    files.readFile(pathRout, 'utf8', (err, data) => {
+            // console.log(data);     
+            lineCount(data)
     }
-    
-});
+    )};
+
+const validatePath = (pathRout) => {
+    readDoc(path.resolve(pathRout));     
 }
-
-
-
-
-//definiendo una ruta absolutas
-const validatePath = (pathrout)=>{
-    // console.log(typeof pathrout);
-    if(!path.isAbsolute(pathrout)) {
-        const urlResult = path.resolve(pathrout)
-         //console.log( urlResult);
-         lineCount(urlResult)
-    }
-}
-
-validatePath('./src/README.md');
-
- 
-
-
-
+validatePath('./README.md');
 
 module.exports = {
     lineCount, 
+    readDoc,
     validatePath,
-    validate
+ 
+    // regularExpersion
+    // validateUrl
+ 
 }; 
+// const lineCount = (urlResult) =>{
+    //     let newUrl = []; 
+    //     files.readFile(urlResult,'utf8',(err, data) =>{
+    //   if (err){
+    //       console.log(err);
+    //       return 'error';
+    //   }else{
+    //       //saltos de linea \n
+    //       const lines = data.split('\n');
+    //       lines.forEach(element => {
+    //       const expresion = new RegExp(/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g);
+    //       while(element.match(expresion) !== null){  
+    //         newUrl.push(element.match(expresion));
+           
+            
+    //         return newUrl;
+    //          } 
+                
+    //         }); 
+    
+    //     // validate(newUrl);
+    //        return lines + ' '+'lines'     
+    //     }
+        
+    // });
+    // }
+// }
+// //definiendo una ruta absolutas
+// const validatePath = (pathrout)=>{
+  
+//     // if(!path.isAbsolute(pathrout)) {
+//         const urlResult = path.resolve(pathrout)
+//          console.log( urlResult);
+//         //  lineCount(urlResult)
+//     }
+// }
+// function validate(newUrl){    
+//     newUrl.forEach(element =>{
+//         console.log(element);
+//     //   fetch(element).then(function(response){
+//     //    console.log(response.status);
+//     // fetch(newUrl).then(response => console.log(response.status))
+//     //   });
+        
+//      })
+// // fetch(newUrl).then(response => console.log(response.status))
+//     }
 
+// const lineCount = (urlResult) =>{
+//     let newUrl = []; 
+//     files.readFile(urlResult,'utf8',(err, data) =>{
+//   if (err){
+//       console.log(err);
+//       return 'error';
+//   }else{
+//       //saltos de linea \n
+//       const lines = data.split('\n');
+//       lines.forEach(element => {
+//       const expresion = new RegExp(/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g);
+//       while(element.match(expresion) !== null){  
+//         newUrl.push(element.match(expresion));
+       
+        
+//         return newUrl;
+//          } 
+            
+//         }); 
+
+//     // validate(newUrl);
+//        return lines + ' '+'lines'     
+//     }
+    
+// });
+// }
+
+// const regularExpersion = (lines) =>{
+// console.log(lines);
+
+// }
+
+
+
+    // // newArray.forEach(element=>{
+    //     fetch(newArray).then((resolve)=>{
+    //         console.log(resolve.status);    
+    //     // })
+    //     // .catch(error)
+    //     // console.log(error);})
